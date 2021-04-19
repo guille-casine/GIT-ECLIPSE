267651fce8dcf7d74dfad6a700eeefdfabe37a8f
Usu2 name <usu2@example.com>
2021-04-19 16:35:51 +0200

    Validacion en cambio1

--- Prueba1/src/prueba1/Principal.java
+++ Prueba1/src/prueba1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String []args) {
 		System.out.println("Hola mundo");
 		System.out.println("Hola otra vez");
+		System.out.println("Hola otra vez x2");
 	}
 }
